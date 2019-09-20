075d8e295f3e1f2fa9c682d325ddb36583c75e9c
Swastik Nath <swastik.nath@live.com>
2019-09-20 11:35:07 +0000

    Implement working signup and login functionality

--- application.py
+++ application.py
@@ -51,11 +51,29 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return render_template("signup.html")
+    if request.method == 'GET':
+        return render_template("signup.html")
+    if request.method == 'POST':
+        session['user'] = User(firstname=request.form.get('firstname'),
+                            middlename=request.form.get('middlename'),
+                            lastname=request.form.get('lastname'),
+                            emailid=request.form.get('emailid'),
+                            password=generate_password_hash(request.form.get('password')).decode('utf-8'),
+                            verified=True)
+        db.session.add(session['user'])
+        db.session.commit()
+        return redirect(url_for('login'))
 
 
-@app.route('/login')
+@app.route('/login',  methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    if request.method == 'GET':
+        return render_template("login.html")
+    if request.method == 'POST':
+        session['user'] = User.query.filter_by(emailid=request.form.get('emailid')).first()
+        if check_password_hash(session['user'].password, request.form.get('password')):
+            session['loggedin'] = True
+            return "login successful"
+        return redirect(url_for('login'))
